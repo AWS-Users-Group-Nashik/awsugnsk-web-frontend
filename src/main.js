3c2a46cb69ebb031c545eb4f08119dd29edab2c2
cyproto <kyashkarande@gmail.com>
2021-06-24 20:33:54 +0100

    api.service: Prepare api service.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import Vuesax from "vuesax";
 import App from "./App.vue";
 import router from "./router";
+import ApiService from "./common/api.service";
 
 import "vuesax/dist/vuesax.css";
 
+ApiService.init();
 Vue.use(Vuesax);
 
 new Vue({
