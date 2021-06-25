9e23586be7e53c9470103a6f69ad6520705d23b4
iamabhishek21 <abhishek@codeops.tech>
2021-06-25 10:40:03 +0530

    minor changes to members component

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,40 +1,40 @@
 <template>
-	<vs-card type="5">
-		<template #title>
-			<h3>{{member.name}}</h3>
-		</template>
-		<template #img>
-			<img
-				src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-				alt="member_photo"
-			/>
-		</template>
-		<template #text>
-			<p>
-				{{member.tagline}}
-			</p>
-		</template>
-		<template #interactions>
-			<vs-button danger icon>
-				<i class="bx bxl-linkedin"></i>
-			</vs-button>
-			<!-- <vs-button class="btn-chat" shadow primary>
-				<i class="bx bx-chat"></i>
-				<span class="span">
-					54
-				</span>
-			</vs-button> -->
-		</template>
-	</vs-card>
+  <vs-card type="5">
+    <template #title>
+      <h2>{{ member.name }}</h2>
+    </template>
+    <template #img>
+      <img
+        src="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+        alt="member_photo"
+      />
+    </template>
+    <template #text>
+      <h3>
+        {{ member.member_type }}
+      </h3>
+      <p>
+        {{ member.bio }}
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class="bx bxl-linkedin"></i>
+      </vs-button>
+      <vs-button primary icon>
+        <i class="bx bxl-twitter"></i>
+      </vs-button>
+    </template>
+  </vs-card>
 </template>
 
 <script>
 export default {
-	name: 'Member',
-	props: {
-		member: Object
-	},
-}
+  name: "Member",
+  props: {
+    member: Object
+  }
+};
 </script>
 
 <style></style>
