2cd425c3e83761a251fa2ce5aa366336e5bc5723
cyproto <kyashkarande@gmail.com>
2021-06-26 18:48:48 +0530

    NoItems: Add new component to be shown in empty sections.

---
+++ src/components/NoItems.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="no-items-wrapper">
+    <span class="no-items-text">No {{ title }} found</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoItems",
+  props: {
+    title: { type: String },
+  },
+};
+</script>
+
+<style>
+.no-items-wrapper {
+  height: 100px;
+  width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+.no-items-text {
+  font-size: 20px;
+  font-weight: bolder;
+  padding: 10px 20px;
+  background: #ffc074;
+  color: white;
+  border-radius: 10px;
+}
+</style>
