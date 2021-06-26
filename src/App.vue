12309b74265171e7301df97668e1d74c14bfcea7
cyproto <kyashkarande@gmail.com>
2021-06-26 18:41:35 +0530

    App: Add footer css and other common css.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <Nav />
-    <router-view></router-view>
-    <Footer />
+    <router-view class="main-content"></router-view>
+    <Footer class="footer" />
   </div>
 </template>
 
@@ -29,6 +29,25 @@ export default {
   font-family: "Avenir";
 }
 
+html,
+body {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+.main-content {
+  margin: 8px;
+  min-height: 100%;
+  height: auto !important;
+  height: 100%;
+  margin: 0 auto 0px;
+}
+
 .container {
   max-width: 1080px;
   margin: auto;
@@ -38,6 +57,9 @@ export default {
   max-width: 1080px;
   margin: auto;
   margin-top: 100px;
+}
+
+.page-heading-title {
   font-size: 50px;
   text-decoration: #ffc074 solid overline;
 }
@@ -46,9 +68,17 @@ export default {
   width: 0;
 }
 
+.footer {
+  height: 60px;
+}
+
 @media screen and (max-width: 768px) {
   .page-heading {
     margin-left: 5vw;
+  }
+
+  .footer {
+    height: 150px;
   }
 }
 </style>
