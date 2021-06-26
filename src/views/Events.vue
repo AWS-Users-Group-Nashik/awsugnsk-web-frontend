64088451366d973c4627f1a8154cb3d53ea6c16b
cyproto <kyashkarande@gmail.com>
2021-06-26 18:52:05 +0530

    Events: Add progress loader and use NoItems.

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
-    <h2 class="page-heading">Events</h2>
+    <h2 class="page-heading page-heading-title">Events</h2>
     <div class="container">
       <vs-row>
         <vs-col lg="4" sm="6" xs="12" v-for="event in events" :key="event.id">
           <Event v-bind="event" />
         </vs-col>
       </vs-row>
+      <div v-if="!events.length && isResponseReceived">
+        <NoItems :title="'events'" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Event from "@/components/Event.vue";
+import NoItems from "@/components/NoItems.vue";
 import ApiService from "@/common/api.service";
 export default {
   name: "Events",
-  components: { Event },
+  components: { Event, NoItems },
   data() {
     return {
       events: [],
       windowWidth: null,
+      isResponseReceived: false,
     };
   },
   created() {
+    const loading = this.$vs.loading({ type: "circles", color: "#ffc074" });
     ApiService.get("/events/").then((response) => {
       this.events = response.data;
+      this.isResponseReceived = true;
+      loading.close();
     });
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin-top: 20px;
+}
+</style>
