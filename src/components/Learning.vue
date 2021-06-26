02f65eb695283625b62f6c6d61d1671395483e06
cyproto <kyashkarande@gmail.com>
2021-06-26 18:43:38 +0530

    Learning: Prepare learning component.

--- src/components/Learning.vue
+++ src/components/Learning.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>Learning {{ id }}</div>
+  <vs-card type="4" class="learning-card">
+    <template #title>
+      <h3>{{ title }}</h3>
+    </template>
+    <template #img>
+      <img :src="image_url" :alt="title" class="learning-image" />
+    </template>
+    <template #text>
+      <p>{{ proficiency }}</p>
+    </template>
+    <template #interactions>
+      <vs-button v-bind:color="buttonColor" icon :href="url" blank circle>
+        <i v-bind:class="buttonIcon"></i>
+      </vs-button>
+    </template>
+  </vs-card>
 </template>
 
 <script>
@@ -7,9 +22,42 @@ export default {
   name: "Learning",
   props: {
     id: { type: Number },
+    title: { type: String },
+    proficiency: { type: String },
+    type: { type: String },
+    url: { type: String },
+    image_url: { type: String },
+  },
+  computed: {
+    buttonColor: function () {
+      return this.type == "Youtube" ? "youtube" : "#171717";
+    },
+    buttonIcon: function () {
+      return this.type == "Youtube" ? "bx bxl-youtube" : "bx bxl-medium";
+    },
   },
 };
 </script>
 
 <style>
+.learning-card {
+  padding: 10px;
+  display: grid;
+  place-items: center;
+}
+
+.learning-image {
+  height: 250px;
+  width: 500px;
+}
+
+.learning-card .vs-card__text {
+  width: 100%;
+  background: none !important;
+  color: white;
+}
+
+.vs-card-content.type-4 .vs-card {
+  max-width: none;
+}
 </style>
