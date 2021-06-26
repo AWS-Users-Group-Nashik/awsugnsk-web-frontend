5a4b060f7df7874be7677db4a06300a894b8d8a7
cyproto <kyashkarande@gmail.com>
2021-06-26 18:54:44 +0530

    Home: Use progress loader and NoItems.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
-    <h2 class="page-heading">Home</h2>
+    <h2 class="page-heading page-heading-title">Stories</h2>
     <div class="container">
       <Story v-for="story in stories" :key="story.id" v-bind="story" />
+    </div>
+    <div v-if="!stories.length && isResponseReceived">
+      <NoItems :title="'stories'" />
     </div>
   </div>
 </template>
 
 <script>
 import Story from "@/components/Story.vue";
+import NoItems from "@/components/NoItems.vue";
 import ApiService from "@/common/api.service";
 export default {
   name: "Home",
-  components: { Story },
+  components: { Story, NoItems },
   data() {
     return {
       stories: [],
+      isResponseReceived: false,
     };
   },
   created() {
+    const loading = this.$vs.loading({ type: "circles", color: "#ffc074" });
     ApiService.get("/stories/").then((response) => {
       this.stories = response.data;
+      this.isResponseReceived = true;
+      loading.close();
     });
   },
 };
