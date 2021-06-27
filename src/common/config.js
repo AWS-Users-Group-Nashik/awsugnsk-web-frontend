5f0a8d5d72278e5731f7b16e374f9fa54c0fe551
cyproto <kyashkarande@gmail.com>
2021-06-27 22:57:19 +0530

    config: Switch to production API.

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,3 @@
 export const API_URL =
-  "https://94xbibgl5h.execute-api.us-east-1.amazonaws.com/dev";
+  "https://ek1tbemoxe.execute-api.ap-south-1.amazonaws.com/production";
 export default API_URL;
