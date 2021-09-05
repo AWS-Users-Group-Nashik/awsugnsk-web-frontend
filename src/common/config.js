b8b7e47d087935ab91632e608a5ec6409764e088
cyproto <kyashkarande@gmail.com>
2021-09-05 16:20:35 +0530

    config: Replace API URL.

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,3 @@
 export const API_URL =
-  "https://ek1tbemoxe.execute-api.ap-south-1.amazonaws.com/production";
+  "https://eyc8xur291.execute-api.ap-south-1.amazonaws.com/production";
 export default API_URL;
